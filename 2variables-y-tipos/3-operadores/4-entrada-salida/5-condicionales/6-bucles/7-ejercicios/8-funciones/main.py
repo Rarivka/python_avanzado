9fdda50d788a3c56a04bc1cd3102e01acfeabf6c
Rarivka <raquelrivascas@gmail.com>
2025-05-20 18:36:45 +0200

    más

---
+++ 2variables-y-tipos/3-operadores/4-entrada-salida/5-condicionales/6-bucles/7-ejercicios/8-funciones/main.py
@@ -0,0 +1,24 @@
+# Ejemplo 1:
+print("########Ejemplo ######")
+
+# Definir funciones
+def muestraNombres():
+    print("Raquel")
+    print("Rebeca")
+    print("Miriam")
+    print("Silvia")
+    print("Natividad")
+    print("\n")
+    
+# Invocar funciones
+muestraNombres()  # Se eliminó el espacio antes de la llamada a la función
+
+# Ejemplo 2: parametro
+# print("########## ejemplo 2 ########")
+def mostrarTuNombre():
+     print(f"Tu nombre es :{nombre}")
+     
+     mostrarTuNombre("Raquel Rivas")
+     mostrarTuNombre("Rebeca")
+     mostrarTuNombre("Esmeralda")
+     
